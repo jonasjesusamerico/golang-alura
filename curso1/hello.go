486ab9866ebe14b1f190802c0afec73f8ac049f2
jonasjesusamerico <jjsamerico@gmail.com>
2022-04-12 21:23:17 -0300

    Primeiro curso da formação golang

---
+++ curso1/hello.go
@@ -0,0 +1,119 @@
+package curso1
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+const monitoramentos = 3
+const delay = 5
+
+func main() {
+	exibeIntroducao()
+	for {
+
+		exibeMenu()
+
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+	}
+
+}
+
+func exibeIntroducao() {
+	var nome string = "Teste"
+	versao := 1.1 // Iferência de tipo, mais comum ser utilizado
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Esta programa está na versão ", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+
+	sites := leSitesDoArquivo()
+
+	// fmt.Println(sites)
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso")
+	} else {
+		fmt.Println("Site:", site, "está com problema. Status Code:", resp.StatusCode)
+	}
+}
+
+func leSitesDoArquivo() []string {
+
+	var sites []string
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+	}
+
+	arquivo.Close()
+	return sites
+
+}
